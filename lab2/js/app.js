7e2a36c7d4bbc43fee9f2ab034318f5ef89e4bf4
Johan Kasperi <johan@kasperi.se>
2016-02-04 15:47:35 +0100

    remover bower_components

--- lab2/js/app.js
+++ lab2/js/app.js
@@ -1,9 +1,8 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel(1, "2016-01-01", 6);
-  console.log(model.getNumberOfGuests());
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
-
+  
 });
