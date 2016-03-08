4e87985ea6a41c3e10ca9d70f6b2ed7e31b1ddea
Johan Kasperi <johan@kasperi.se>
2016-03-08 20:00:24 +0100

    added the dinner view and controller

---
+++ lab5/app/js/dinnerCtrl.js
@@ -0,0 +1,28 @@
+// Dinner controller that we use whenever we have view that needs to 
+// display or modify the dinner menu
+dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
+
+  $scope.getNumberOfGuests = function() {
+    return Dinner.getNumberOfGuests();
+  }
+
+  $scope.getDate = function() {
+    return Dinner.getDate();
+  }
+
+  $scope.getPrice = function() {
+    return Dinner.getTotalMenuPrice();
+  }
+
+  $scope.getDishes = function() {
+    return Dinner.getFullMenu();
+  }
+
+  $scope.getDishPrice = function(id) {
+    return Dinner.getDishPrice(id);
+  }
+
+  // TODO in Lab 5: Implement the methods to get the dinner menu
+  // add dish to menu and get total menu price
+
+});
