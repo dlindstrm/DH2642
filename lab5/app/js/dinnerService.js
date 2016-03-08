5dd488c20070e857dfca40702b156181326bdf15
Johan Kasperi <johan@kasperi.se>
2016-03-08 16:09:47 +0100

    added underscore module and pasted the dinnermodel code from lab4

--- lab5/app/js/dinnerService.js
+++ lab5/app/js/dinnerService.js
@@ -3,28 +3,104 @@
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
-dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
-  var numberOfGuest = 2;
+dinnerPlannerApp.factory('Dinner',function ($resource, _) {
 
+  var date = new Date();
+  var guests = 1;
+  var dishes = [];
+
+  this.setDate = function(_date) {
+    date = _date;
+  }
+
+  this.getDate = function() {
+    return date;
+  }
+
+  this.getFormattedDate = function() {
+    return date.toISOString().substring(0, 10);
+  }
 
   this.setNumberOfGuests = function(num) {
-    numberOfGuest = num;
+    guests = num;
   }
 
+  // should return 
   this.getNumberOfGuests = function() {
-    return numberOfGuest;
+    return guests;
   }
 
+  //Returns the dish that is on the menu for selected type 
+  this.getSelectedDish = function(type) {
+    return _.find(dishes, function(dish){ return dish.type == type; });
+  }
 
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
+  //Returns all the dishes on the menu.
+  this.getFullMenu = function() {
+    return dishes;
+  }
 
+  //Returns all ingredients for all the dishes on the menu.
+  this.getAllIngredients = function() {
+    var ingredients = [];
+    for(i in dishes) {
+      for(j in dishes[i].ingredients) {
+        ingredients.push(dishes[i].ingredients[j]);
+      }
+    }
+    return ingredients;
+  }
 
+  //Returns the total price of the menu (all the ingredients multiplied by number of guests).
+  this.getTotalMenuPrice = function() {
+    var ingredients = this.getAllIngredients();
+    var price = 0;
+    for(i in ingredients) {
+      price += ingredients[i].price * guests;
+    }
+    return price;
+  }
 
+  this.getDishPrice = function(id) {
+    var dish = _.find(dishes, function(dish){ return dish.id == id; });
+    var price = 0;
+    for(var i=0;i<dish.ingredients.length;i++) {
+      price = price + dish.ingredients[i].price * guests;
+    }
+    return price;
+  }
+
+  //Adds the passed dish to the menu. If the dish of that type already exists on the menu
+  //it is removed from the menu and the new one added.
+  this.addDishToMenu = function(id) {
+    this.removeDishFromMenu(id);
+    var _this = this;
+    this.getDish(id, function(error, dish) {
+      if(error) {
+        return;
+      }
+      dishes.push(dish);
+    })
+  }
+
+  //Removes dish from menu
+  this.removeDishFromMenu = function(id) {
+    dishes = _.reject(dishes, function(obj){
+      return obj.id == id 
+    });
+  }
+
+  //function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
+  //you can use the filter argument to filter out the dish by name or ingredient (use for search)
+  //if you don't pass any filter all the dishes will be returned
+  this.getAllDishes = function (type, filter) {
+    
+  }
+
+  //function that returns a dish of specific ID
+  this.getDish = function (id, callback) {
+
+  }
 
 
   // Angular service needs to return an object that has all the
